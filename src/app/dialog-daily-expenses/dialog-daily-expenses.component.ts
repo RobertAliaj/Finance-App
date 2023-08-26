aadc5d1306ff52f673183998544cb5d549c66e9f
rOBI <robertaliaj97@gmail.com>
2023-08-26 11:59:43 +0200

    added the Json class for Firebase

--- src/app/dialog-daily-expenses/dialog-daily-expenses.component.ts
+++ src/app/dialog-daily-expenses/dialog-daily-expenses.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Firestore, addDoc, collection, getDoc } from '@angular/fire/firestore';
+import { Expenses } from 'src/models/expenses.class';
+
 
 @Component({
   selector: 'app-dialog-daily-expenses',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./dialog-daily-expenses.component.scss']
 })
 export class DialogDailyExpensesComponent {
+
+
+  expenses: Expenses = new Expenses();
+
+
+  constructor(private firestore: Firestore){
+    console.log(this.expenses);
+  }
+
+
+
+  async addTransaction() {
+    const transactionsCollection = collection(this.firestore, 'transactions');
+    // addDoc(transactionsCollection, this.transaction.toJson()).then(async (result) => {
+      // const docSnap = await getDoc(result);
+    // });
+  }
 
 }
